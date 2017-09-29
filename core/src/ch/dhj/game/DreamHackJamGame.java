0eadead563c7c6bf4dfff28a0c3092083976b3b1
MCPGamer <Duarte.amarelo@hotmail.com>
2017-09-30 00:23:51 +0200

    Added MainMenuScreen (Work in Progress)

--- core/src/ch/dhj/game/DreamHackJamGame.java
+++ core/src/ch/dhj/game/DreamHackJamGame.java
@@ -1,15 +1,18 @@
 package ch.dhj.game;
 
+import ch.dhj.game.screens.MainMenu;
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class DreamHackJamGame extends ApplicationAdapter {
+public class DreamHackJamGame extends Game {
 	
 	@Override
 	public void create () {
+		this.setScreen(new MainMenu());
 	}
 
 	@Override
