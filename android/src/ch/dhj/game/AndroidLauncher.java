ed281b666927c9c75f6210a6699eb46818971073
Sebastian <sebastian.zumbrunn@gmail.com>
2017-09-29 19:21:53 +0200

    created libgdx project

---
+++ android/src/ch/dhj/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ch.dhj.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ch.dhj.game.DreamHackJamGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DreamHackJamGame(), config);
+	}
+}
---
+++ desktop/src/ch/dhj/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ch.dhj.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ch.dhj.game.DreamHackJamGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DreamHackJamGame(), config);
+	}
+}
