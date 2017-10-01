f917e57d08f4d335cdcb7df38a058a4bf9f35b83
Sebastian <sebastian.zumbrunn@gmail.com>
2017-10-01 21:03:57 +0200

    added do nothing button

--- core/src/ch/dhj/game/encounter/obj/objects/Player.java
+++ core/src/ch/dhj/game/encounter/obj/objects/Player.java
@@ -189,11 +189,11 @@ public class Player extends Figure{
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
 				if(selectedWeapon.isSpell()) { //weapon is a spell
-					addActionToTurn(new RangeWeaponAction(selectedWeapon, Player.this, new Enemy[]{enemies.getSelected()}));
+					addActionToTurn(new RangeWeaponAction(selectedWeapon, Player.this, new Enemy[]{enemies.getSelected()}), selectedWeapon.getActionCost());
 				} else if(selectedWeapon.isMelee()) { //weapon is a mele attack
-					addActionToTurn(new MeleeWeaponAction(selectedWeapon, Player.this, new Enemy[]{enemies.getSelected()}));
+					addActionToTurn(new MeleeWeaponAction(selectedWeapon, Player.this, new Enemy[]{enemies.getSelected()}), selectedWeapon.getActionCost());
 				} else { //weapon is a range weapon
-					addActionToTurn(new RangeWeaponAction(selectedWeapon, Player.this, new Enemy[]{enemies.getSelected()}));
+					addActionToTurn(new RangeWeaponAction(selectedWeapon, Player.this, new Enemy[]{enemies.getSelected()}), selectedWeapon.getActionCost());
 				}
 				chooseSpellTable.setVisible(false);
 			}
@@ -210,7 +210,7 @@ public class Player extends Figure{
 			public void clicked(InputEvent event, float x, float y) {
 				Weapon w = weapons.getSelected();
 				if(w.isMultipleTargets()) {
-					addActionToTurn(new MeleeWeaponAction(getMeleeWeapon(), Player.this, getEncounterConfig().enemies.toArray()));
+					addActionToTurn(new MeleeWeaponAction(getMeleeWeapon(), Player.this, getEncounterConfig().enemies.toArray()), w.getActionCost());
 					return;
 				}
 				selectedWeapon = w;
@@ -233,14 +233,6 @@ public class Player extends Figure{
 		attackB.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
-				//
-				/*Weapon w = Player.this.getMeleeWeapon();
-				if(w.isMultipleTargets()) {
-					addActionToTurn(new MeleeWeaponAction(getMeleeWeapon(), Player.this, getEncounterConfig().enemies.toArray()));
-					return;
-				}
-				selectedWeapon = w;
-				chooseEnemyTable.setVisible(!chooseEnemyTable.isVisible());*/
 				if(getMeleeWeapon() != null)
 					weapons.setItems(getMeleeWeapon());
 				if(getRangeWeapon() != null)
@@ -258,11 +250,15 @@ public class Player extends Figure{
 				chooseEnemyTable.setVisible(false);
 			}
 		});
-		TextButton defendB = new TextButton("Defend", skin);
-		defendB.addListener(new ClickListener() {
+		TextButton doNothingB = new TextButton("Do nothing", skin);
+		doNothingB.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
-
+				actions++;
+				actionsL.setText(String.format("%d/%d Actions", actions, getMaxActionCount()));
+				if(actions >= getMaxActionCount()) {
+					endTurn();
+				}
 			}
 		});
 		TextButton runB = new TextButton("Run", skin);
@@ -327,7 +323,7 @@ public class Player extends Figure{
 		turnActionTable.row();
 		turnActionTable.add(spellB).width(200).height(50);
 		turnActionTable.row();
-		turnActionTable.add(defendB).width(200).height(50);
+		turnActionTable.add(doNothingB).width(200).height(50);
 		turnActionTable.row();
 		turnActionTable.add(runB).width(200).height(50);
 
@@ -339,9 +335,9 @@ public class Player extends Figure{
 		//rootTable.setY(rootTable.getHeight()/2);
 	}
 
-	private void addActionToTurn(Action a) {
+	private void addActionToTurn(Action a, int neededActions) {
 		currentTurn.addAction(a);
-		actions++;
+		actions += neededActions;
 		actionsL.setText(String.format("%d/%d Actions", actions, getMaxActionCount()));
 		chooseEnemyTable.setVisible(false);
 		if(actions >= getMaxActionCount()) {
